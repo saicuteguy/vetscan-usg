e33ca7e86f56d1f4a66c8a9150712e8c75c78706
vetsmart <saicuteguy@gmail.com>
2026-07-12 19:32:01 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,32 @@
-// Uninstall service worker
-self.addEventListener('install', function(){ self.skipWaiting(); });
-self.addEventListener('activate', function(e){
+var CACHE = "pradarsh-v7";
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
   e.waitUntil(
-    caches.keys().then(function(k){
-      return Promise.all(k.map(function(c){ return caches.delete(c); }));
+    caches.open(CACHE).then(function(c){
+      return c.addAll(["/Pradarsh/", "/Pradarsh/index.html"]).catch(function(){});
+    })
+  );
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
     }).then(function(){ return self.clients.claim(); })
   );
 });
-self.addEventListener('fetch', function(e){
-  e.respondWith(fetch(e.request));
+
+self.addEventListener("fetch", function(e){
+  e.respondWith(
+    fetch(e.request).then(function(r){
+      var c = r.clone();
+      caches.open(CACHE).then(function(cache){ cache.put(e.request, c); });
+      return r;
+    }).catch(function(){
+      return caches.match(e.request).then(function(r){
+        return r || caches.match("/Pradarsh/index.html");
+      });
+    })
+  );
 });
